98761baaad48c7e297d00c5f188fe86db9826a96
Tommaso Pardi <tommaso.pardi@gmail.com>
2023-10-13 10:05:20 +0100

    feat: added bodycontainer to pages

--- src/pages/FabrikPage.js
+++ src/pages/FabrikPage.js
@@ -1,13 +1,16 @@
 import Robot from '../Components/Robot'
 import Header from "../Components/Header";
+import styles from '../styles/Global.module.scss'
 
 export const FabrikPage = ({ menu }) => {
   return (
     <>
       <Header menu={menu}/>
-      <h1>Fabrik</h1>
-      <p>FabrikFabrikFabrikFabrik</p>
-      <Robot />
+      <div className={styles.bodyContainer}>
+        <h1>Fabrik</h1>
+        <p>FabrikFabrikFabrikFabrik</p>
+        <Robot />
+      </div>
     </>
   )
 }
--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,11 +1,14 @@
 import Header from "../Components/Header";
+import styles from '../styles/Global.module.scss'
 
 export const NoPage = ({ menu }) => {
     return (
       <>
       <Header menu={menu}/>
-      <h1>Error 404</h1>
-      <p>his page doesn't exist</p>
+      <div className={styles.bodyContainer}>
+        <h1>Error 404</h1>
+        <p>This page doesn't exist</p>
+      </div>
       </>
     )
   }
